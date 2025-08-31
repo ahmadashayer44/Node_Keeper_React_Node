566bcb1e3474151aa96cb0b80ee788771a91435f
ahmadashayer44 <ashayera44@gmail.com>
2025-08-31 22:41:40 +0300

    Getting notes from backend implemented

--- server/app.js
+++ server/app.js
@@ -4,7 +4,9 @@ const db = require("./db.js").db;
 const mongoose = require("mongoose");
 const app = express();
 const port = 3001;
+const cors = require("cors");
 
+app.use(cors());
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
 });
